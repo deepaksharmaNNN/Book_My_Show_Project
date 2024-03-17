61bba54c175b02e928cb37f841b6bde6c4e5bd00
Deepak Sharma <dsharma2828@gmail.com>
2024-03-17 13:15:07 +0530

    Project Creation

---
+++ src/main/java/com/deepaksharma/book_my_show_project/BookMyShowProjectApplication.java
@@ -0,0 +1,13 @@
+package com.deepaksharma.book_my_show_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMyShowProjectApplication.class, args);
+    }
+
+}
