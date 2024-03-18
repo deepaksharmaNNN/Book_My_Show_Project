310fe301595a3b188a272ff03fd5b3a55801396b
Deepak Sharma <dsharma2828@gmail.com>
2024-03-18 11:27:30 +0530

    Connected Database and created Movie Entity

---
+++ src/main/java/com/deepaksharma/book_my_show_project/Entities/Movie.java
@@ -0,0 +1,19 @@
+package com.deepaksharma.book_my_show_project.Entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "movies")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String movieId;
+
+
+    @Column(unique = true, nullable = false, length = 100)
+    private String movieName;
+
+
+}
