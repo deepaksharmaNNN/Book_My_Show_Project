9b8ed89f2cad035dcdd26d5c81308eefc5823e59
Deepak Sharma <dsharma2828@gmail.com>
2024-03-19 11:52:01 +0530

    connected showseats with show entity by mappings

--- src/main/java/com/deepaksharma/book_my_show_project/Entities/Show.java
+++ src/main/java/com/deepaksharma/book_my_show_project/Entities/Show.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
@@ -29,4 +31,8 @@ public class Show {
     @JoinColumn
     @ManyToOne
     private Theater theater;
+
+    @OneToMany(mappedBy = "show", cascade = CascadeType.ALL)
+    private List<ShowSeat> showSeatsList = new ArrayList<>();
+
 }
--- src/main/java/com/deepaksharma/book_my_show_project/Entities/ShowSeat.java
+++ src/main/java/com/deepaksharma/book_my_show_project/Entities/ShowSeat.java
@@ -1,2 +1,32 @@
-package com.deepaksharma.book_my_show_project.Entities;public class ShowSeat {
+package com.deepaksharma.book_my_show_project.Entities;
+
+import com.deepaksharma.book_my_show_project.Enums.SeatType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "show_seats")
+public class ShowSeat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer showSeatId;
+
+    private Integer price;
+
+    private Boolean isAvailable;
+
+    private Boolean foodIncluded;
+
+    private Integer seatNumber;
+    //will get these values from mappings;
+    private SeatType seatType;
+
+    @JoinColumn
+    @ManyToOne
+    private Show show;
 }
