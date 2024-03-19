33160b1e7936345c7dc39feeb2ea86fd56062091
Deepak Sharma <dsharma2828@gmail.com>
2024-03-19 10:51:42 +0530

    Made changes in Movie Entity

--- src/main/java/com/deepaksharma/book_my_show_project/Entities/Movie.java
+++ src/main/java/com/deepaksharma/book_my_show_project/Entities/Movie.java
@@ -1,11 +1,18 @@
 package com.deepaksharma.book_my_show_project.Entities;
 
 import com.deepaksharma.book_my_show_project.Enums.Genre;
+import com.deepaksharma.book_my_show_project.Enums.Language;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "movies")
 public class Movie {
     @Id
@@ -16,6 +23,15 @@ public class Movie {
     @Column(unique = true, nullable = false, length = 100)
     private String movieName;
 
+    @Enumerated(value = EnumType.STRING)
     private Genre genre;
+
+    @Enumerated(value = EnumType.STRING)
+    private Language language;
+
+    private LocalDate releaseDate;
+
+    private Double duration;
+
 
 }
