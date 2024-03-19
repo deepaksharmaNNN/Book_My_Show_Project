c546854e280601a40dbdc1b56172d1ad2a0510c9
Deepak Sharma <dsharma2828@gmail.com>
2024-03-19 10:22:43 +0530

    added Language Enum

---
+++ src/main/java/com/deepaksharma/book_my_show_project/Enums/Language.java
@@ -0,0 +1,25 @@
+package com.deepaksharma.book_my_show_project.Enums;
+
+public enum Language {
+    ENGLISH,
+    HINDI,
+    TAMIL,
+    TELUGU,
+    KANNADA,
+    MALAYALAM,
+    BENGALI,
+    PUNJABI,
+    GUJARATI,
+    MARATHI,
+    URDU,
+    ODIA,
+    ASSAMESE,
+    RAJASTHANI,
+    BHOJPURI,
+    HARYANVI,
+    CHHATTISGARHI,
+    KASHMIRI,
+    NEPALI,
+    SINDHI,
+    SANSKRIT
+}
