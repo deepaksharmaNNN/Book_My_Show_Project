b7ea959f94e297ba2752293b3c32f6ca15320f3f
Deepak Sharma <dsharma2828@gmail.com>
2024-04-01 13:16:25 +0530

    Added Builder Annotation and used it in movie service class

--- src/main/java/com/deepaksharma/book_my_show_project/Entities/Movie.java
+++ src/main/java/com/deepaksharma/book_my_show_project/Entities/Movie.java
@@ -4,6 +4,7 @@ import com.deepaksharma.book_my_show_project.Enums.Genre;
 import com.deepaksharma.book_my_show_project.Enums.Language;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.util.List;
 
 @Entity
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "movies")
--- src/main/java/com/deepaksharma/book_my_show_project/Services/MovieService.java
+++ src/main/java/com/deepaksharma/book_my_show_project/Services/MovieService.java
@@ -1,11 +1,18 @@
 package com.deepaksharma.book_my_show_project.Services;
 
+import com.deepaksharma.book_my_show_project.Entities.Movie;
 import com.deepaksharma.book_my_show_project.RequestDTOs.AddMovieRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MovieService {
     public String addMovie(AddMovieRequest addMovieRequest){
-        return "Movie added successfully";
+        Movie movie = Movie.builder()
+                .movieName(addMovieRequest.getMovieName())
+                .genre(addMovieRequest.getGenre())
+                .language(addMovieRequest.getLanguage())
+                .releaseDate(addMovieRequest.getReleaseDate())
+                .duration(addMovieRequest.getDuration())
+                .build();
     }
 }
