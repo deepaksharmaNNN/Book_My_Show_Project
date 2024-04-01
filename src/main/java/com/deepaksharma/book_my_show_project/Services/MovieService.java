0c8b8f37fe6fda305f98019cc141b5e4e9fcef52
Deepak Sharma <dsharma2828@gmail.com>
2024-04-01 17:51:34 +0530

    pause for today

--- src/main/java/com/deepaksharma/book_my_show_project/Services/MovieService.java
+++ src/main/java/com/deepaksharma/book_my_show_project/Services/MovieService.java
@@ -1,11 +1,16 @@
 package com.deepaksharma.book_my_show_project.Services;
 
 import com.deepaksharma.book_my_show_project.Entities.Movie;
+import com.deepaksharma.book_my_show_project.Repository.MovieRepository;
 import com.deepaksharma.book_my_show_project.RequestDTOs.AddMovieRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+
     public String addMovie(AddMovieRequest addMovieRequest){
         Movie movie = Movie.builder()
                 .movieName(addMovieRequest.getMovieName())
@@ -14,5 +19,7 @@ public class MovieService {
                 .releaseDate(addMovieRequest.getReleaseDate())
                 .duration(addMovieRequest.getDuration())
                 .build();
+        movieRepository.save(movie);
+        return "Movie added successfully With ID: "+movie.getMovieId();
     }
 }
