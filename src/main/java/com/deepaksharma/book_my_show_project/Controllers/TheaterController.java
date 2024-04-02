d10ef950a392c24d7d8e889cbf2fb2e8ad0f2b0d
Deepak Sharma <dsharma2828@gmail.com>
2024-04-02 12:13:40 +0530

    completed addTheater as for now

--- src/main/java/com/deepaksharma/book_my_show_project/Controllers/TheaterController.java
+++ src/main/java/com/deepaksharma/book_my_show_project/Controllers/TheaterController.java
@@ -1,6 +1,8 @@
 package com.deepaksharma.book_my_show_project.Controllers;
 
 import com.deepaksharma.book_my_show_project.RequestDTOs.AddTheaterRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/theater")
 public class TheaterController {
     @PostMapping("/addTheater")
-    public String addTheater(@RequestBody AddTheaterRequest addTheaterRequest){
-        return "Theater added successfully";
+    public ResponseEntity addTheater(@RequestBody AddTheaterRequest addTheaterRequest){
+        return new ResponseEntity<>("Theater added successfully", HttpStatus.OK);
     }
 }
