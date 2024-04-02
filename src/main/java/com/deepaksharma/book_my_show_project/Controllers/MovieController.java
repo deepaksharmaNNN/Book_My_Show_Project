c2a79f3fde2e031ccbd523a89149a60da916e967
Deepak Sharma <dsharma2828@gmail.com>
2024-04-02 11:38:34 +0530

    addMovie Annotation is done

--- src/main/java/com/deepaksharma/book_my_show_project/Controllers/MovieController.java
+++ src/main/java/com/deepaksharma/book_my_show_project/Controllers/MovieController.java
@@ -1,6 +1,10 @@
 package com.deepaksharma.book_my_show_project.Controllers;
 
 import com.deepaksharma.book_my_show_project.RequestDTOs.AddMovieRequest;
+import com.deepaksharma.book_my_show_project.Services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
+    @Autowired
+    private MovieService movieService;
     @PostMapping("/addMovie")
-    public String addMovie(@RequestBody AddMovieRequest addMovieRequest){
-        return "Movie added successfully";
+    public ResponseEntity addMovie(@RequestBody AddMovieRequest addMovieRequest){
+        String result = movieService.addMovie(addMovieRequest);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
 }
